75c069a3090628d5d3300cb90d4e2b9caa140ba3
Josh <josh.handon@gmail.com>
2021-05-04 21:30:05 -0400

    Model update

--- app/models/api.rb
+++ app/models/api.rb
@@ -5,4 +5,15 @@ class Api < ActiveResource::Base
 
     #self.auth_type = :bearer
     #self.bearer_token = '54b6a98d972c42f2ae857033bc896c8b'
+
+    def self.get_data
+
+        games_data = RestClient::Request.execute(
+            :url => 'https://api.rawg.io/api/games?54b6a98d972c42f2ae857033bc896c8b',
+            :method => :get,
+            :key => '54b6a98d972c42f2ae857033bc896c8b',
+
+        )
+         JSON.parse(games_data)["results"]
+    end
 end
